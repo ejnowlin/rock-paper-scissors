da97b414c03f3b574337d90e689c7c07bb379d91
Erik Nowlin <4080942+ejnowlin@users.noreply.github.com>
2024-10-26 13:19:59 -0700

    feat: Add function to play a single round

--- script.js
+++ script.js
@@ -28,3 +28,31 @@ function getHumanChoice() {
         return getHumanChoice();
     }
 }
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log("You lose! Paper beats Rock.")
+        ++computerScore;
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log("You win! Rock beats Scissors.");
+        ++humanScore;
+    } else if (humanChoice === "rock" && computerChoice === "rock") {
+        console.log("Tie! Rock twinsies. Nobody scores.");
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log("You win! Paper beats Rock.");
+        ++humanScore;
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        console.log("You lose! Scissors beats Paper.");
+        ++computerScore;
+    } else if (humanChoice === "paper" && computerChoice === "paper") {
+        console.log("Tie! Paper twinsies. Nobody scores.");
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        console.log("You lose! Rock beats Scissors.");
+        ++computerScore;
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log("You win! Scissors beats Paper.");
+        ++humanScore;
+    } else {
+        console.log("Tie! Scissors twinsies. Nobody scores.");
+    }
+}
