091c9d26b533cfe1303c0daf0f86ee5e44af3594
Erik Nowlin <4080942+ejnowlin@users.noreply.github.com>
2024-10-26 13:06:08 -0700

    feat: Add function to get random computer choice

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ function getRandomIntInclusive(min, max) {
     const maxFloored = Math.floor(max);
     return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
 }
+
+function getComputerChoice() {
+    const randomInt = getRandomIntInclusive(1, 9);
+
+    if (randomInt >= 1 && randomInt <= 3) {
+        return "rock";
+    } else if (randomInt >= 4 && randomInt <= 6) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
